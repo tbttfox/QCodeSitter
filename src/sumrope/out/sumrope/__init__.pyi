57708957415c7e955d5e8e983a9274a09bb50fbc
Tyler Fox <tbttfox@gmail.com>
2025-11-29 22:51:03 -0800

    Remove the old stuff

--- src/sumrope/out/sumrope/__init__.pyi
+++
@@ -1,4 +0,0 @@
-from .sumrope import IntGroup as IntGroup, SumRope as SumRope
-from .sumrope_document import SumRopeDocument as SumRopeDocument
-
-__all__ = ['SumRope', 'SumRopeDocument', 'IntGroup']
