32d90a3d5d6f1be0e6679d52901748dd85a88c41
Tyler Fox <tbttfox@gmail.com>
2025-12-02 09:31:26 -0800

    Remove the old highlighter

--- src/sumrope/__init__.py
+++ src/sumrope/__init__.py
@@ -1,5 +1,6 @@
 from .line_editor import CodeEditor
-from .line_tracker import TrackedDocument, SyntaxHighlighter
+from .line_tracker import TrackedDocument
+from .line_highlighter import TreeSitterHighlighter
 from .tree_manager import TreeManager
 from .syntax_analyzer import SyntaxAnalyzer
 from .hl_groups import FORMAT_SPECS
@@ -8,7 +9,7 @@ __all__ = [
     "CodeEditor",
     "FORMAT_SPECS",
     "SyntaxAnalyzer",
-    "SyntaxHighlighter",
+    "TreeSitterHighlighter",
     "TrackedDocument",
     "TreeManager",
 ]
