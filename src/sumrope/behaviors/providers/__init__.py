75c631b990a5bf50b65dc87f4640b5c9307c50a0
Tyler Fox <tbttfox@gmail.com>
2025-12-13 21:13:56 -0800

    Clean up the providers, and dd the python __main__ provider

--- src/sumrope/behaviors/providers/__init__.py
+++ src/sumrope/behaviors/providers/__init__.py
@@ -1,10 +1,13 @@
 from __future__ import annotations
-from ..tab_completion import Completion, TabCompletion
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from ..tab_completion import Completion, TabCompletion
 
 
 class Provider:
     def __init__(self, tabcomplete: TabCompletion):
-        self.tabcomplete = tabcomplete
+        self.tabcomplete: TabCompletion = tabcomplete
 
     def provide(self) -> set[Completion]:
         raise NotImplementedError("A Provider must override the .provide() method")
